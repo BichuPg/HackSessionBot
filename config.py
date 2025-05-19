8e54c9c152cd039f829cce209ba699d403308246
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-05-20 00:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 28350844 
+    API_HASH = "d38cfc3c84a96ea8c59383423e3d9faa"
+    TOKEN = "5497421359:AAF2WNLQAYbiw55oJIalT5rZxTUrhHj2wCU"  
+    START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg" 
+    CHAT = "t.me/anmol0700"    
